692921fc7ec47550f52b09546861924618cfa0a5
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-22 12:00:38 +0300

    Initial Commit

---
+++ Simple browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple browser
+//
+//  Created by Ekaterina Akchurina on 22.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
